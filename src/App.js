36ceff351fdc6432e18f27a5dfb293d4b1aba57f
Cristina Ferrer <krishee107@gmail.com>
2023-04-14 10:53:50 +0200

    Ej 2 : Mostrar las frases pasándolo como prop

--- src/App.js
+++ src/App.js
@@ -3,10 +3,16 @@ import Escena from './components/escena/Escena';
 
 function App() {
 
+  const frases = [
+    "El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial",
+    "Sentia curiositat per l'interior de la nau i es va posar a inspeccionar-la. Va arribar a una sala amb dues portes.",
+    "L'heroi va decidir travessar la porta que el portava a casa",
+    "Mentrestant, altres herois no van tenir tanta sort en la seva elecció …"
+  ];
 
   return (
     <>
-      <Escena />
+      <Escena frase={frases} />
     </>
   );
 }
--- src/components/escena/Escena.js
+++ src/components/escena/Escena.js
@@ -1,3 +1,13 @@
-const Escena = _ => "El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial";
+const Escena = (props) => {
+    const frases = props.frase;
+    const listaFrases = frases.map((frase, index) =>
+        <div key={index}>{frase}</div>
+    );
+    return (
+        <>
+            {listaFrases}
+        </>
+    );
+}
 
 export default Escena;
